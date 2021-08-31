2f54bb2d106c378061638112750c70913f56a4e5
BenJilks <benjyjilks@gmail.com>
2021-08-31 08:59:45 +0000

    Report if we couldn't connect to service

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -5,8 +5,15 @@ use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>>
 {
-    let mut client = Client::new()?;
-    client.send(Command::Exit)?;
+    let client_or_error = Client::new();
+    if client_or_error.is_err() 
+    {
+        println!("Error: Could not connect to service");
+        return Ok(());
+    }
 
+    let mut client = client_or_error.unwrap();
+    client.send(Command::Exit)?;
     Ok(())
 }
+
